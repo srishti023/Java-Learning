e02bec44ef1cbfad98a0946e0fe9803a607a40be
Srishti <srishtichoudhary52@gmail.com>
2025-07-14 00:04:15 +0530

    palindrome program

---
+++ palindrome.java
@@ -0,0 +1,22 @@
+public class palindrome {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("abcdcba");
+        System.out.println(isPalindrome(str));
+        
+    }
+    static boolean isPalindrome(StringBuilder str)
+    {
+        int start =0;
+        int end = str.length()-1;
+        while(start<=end)
+        {
+            if(str.charAt(start)!=str.charAt(end))
+            {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
