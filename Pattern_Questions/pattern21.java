dbac8897e215d9c4c9f799b50a2b053e0932362b
Srishti <srishtichoudhary52@gmail.com>
2025-07-20 22:04:50 +0530

    update pattern 21

--- Pattern_Questions/pattern21.java
+++ Pattern_Questions/pattern21.java
@@ -1,3 +1,11 @@
+// PATTERN :
+
+// 1        
+// 2  3     
+// 4  5  6  
+// 7  8  9  10    
+// 11 12 13 14 15 
+
 public class pattern21 {
     public static void main(String[] args) {
         patternn21(5,1);
