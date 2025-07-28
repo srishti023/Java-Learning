6089160835caa4bcef8d9ec305145515a16fd916
Srishti <srishtichoudhary52@gmail.com>
2025-07-28 22:09:54 +0530

    linear search with arraylist for mulyiple times a element comming

--- Recursion_Questions/linear_search.java
+++ Recursion_Questions/linear_search.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class linear_search {
     public static void main(String[] args) {
-        int []arr = {1,4,2,6,7};
-        System.out.println(linearSearch(arr, 7, 0));
+        int []arr = {1,7,4,2,7,6,7,7};
+        ArrayList <Integer> arl = new ArrayList<>();
+        System.out.println(linear_search(arr, 7, 0, arl));
     }
     static int linearSearch(int []arr, int n, int index)
     {
@@ -29,6 +30,6 @@ public class linear_search {
         {
             list.add(index);
         }
-        linear_search(arr, n, index+1, list);
+        return linear_search(arr, n, index+1, list);
     }
 }
